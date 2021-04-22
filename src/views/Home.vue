386619fd869c0f5f15d462b9626b1a94ac83fec8
pointblank <pointblank.connect@gmail.com>
2021-04-22 09:12:37 -0700

    edits

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,13 +9,7 @@
       @copied="copiedData">
       <img class="copy-icon" src="~/@/assets/favicon.png" width="48px" height="48px" style="" alt="$RHO">
       </vh-copy>
- <vh-copy class="sale-button"
-      :data="'0x51528858b5F7E37aC5976F2E09a07cF2F63Fd664'"
-      :label="'Sale'"
-      :confirm_dialog="'buefy'"
-      @copied="copiedData">
-      <img class="copy-icon" src="~/@/assets/wallet.png" width="48px" height="48px" style="" alt="$BETA">
-      </vh-copy>
+ 
 <jj-parallax-card class="mt-6 para-desktop" lightColor="transparent" style="margin:auto;float:left; width: 512px; height: 450px;" @click="click">
       <div slot="front">
       <img src="~/@/assets/RHO.svg" width="512px;" class="" alt="">
