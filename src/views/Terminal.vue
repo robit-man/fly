d1c970ed88fb7173e79983e6c80b522d2c388a8a
robit <36677806+robit-man@users.noreply.github.com>
2022-06-03 21:37:44 +0000

    fly

--- src/views/Terminal.vue
+++ src/views/Terminal.vue
@@ -8,7 +8,13 @@
     <p style="width:400px;left:30%;position:absolute;font-size:1rem;">There’s literally nothing else like it in the 
 world. Whether you’re traveling domestically or internationally, Fly sets the new standard for traveling experience.</p>
     </div>
-<div class="plane-about"><img src="~/@/assets/about-plane.png" alt=""></div>
+    <div class="plane-about"><img src="~/@/assets/about-plane.png" alt=""></div>
+
+    <div class="cta" style="">
+      <div class="bookbutton">
+        BOOK NOW
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,6 +38,11 @@ export default {
 @keyframes zoom{
   0%{transform:scale(0)translatey(-30%)translatex(-80%);}
   100%{transform:scale(1)translatey(0%)translatex(0%);}
+}
+.cta{width:300px;bottom:3rem;left:calc(50% - 150px);position:fixed;display:flex;flex-flow:row;justify-content:center;}
+.bookbutton{width:150px;height:auto;border-radius:2rem;padding:0.5rem;border:1px solid #ed1c24;color:#ed1c24;text-align:center;font-weight:bolder;
+}
+.bookbutton:hover{color:white;background-color:#ed1c24;cursor:pointer;
 }
 .plane-about{width:calc(100vw - 8rem);left:4rem;position:absolute;bottom:10%;animation:zoom 1s ease-out;}
 .about-fly{position:fixed;left:3rem;height:8rem;width:500px;height:300px;top:calc(30% + 6rem);padding-right:3rem;}
